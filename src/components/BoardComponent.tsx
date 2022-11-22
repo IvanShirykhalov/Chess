53604c51b4b951c426e5beaff188694b95a3ae25
Ivan Shirykhalov <ivanshirykhalov@gmail.com>
2022-11-22 23:06:15 +0300

    highlightCells

--- src/components/BoardComponent.tsx
+++ src/components/BoardComponent.tsx
@@ -1,6 +1,7 @@
-import React, {FC, Fragment} from 'react';
+import React, {FC, Fragment, useState} from 'react';
 import {Board} from "../models/Board";
 import {CellComponent} from "./CellComponent";
+import {Cell} from "../models/Cell";
 
 interface BoardProps {
     board: Board
@@ -8,14 +9,35 @@ interface BoardProps {
 }
 
 export const BoardComponent: FC<BoardProps> = ({board, setBoard}) => {
+
+    const [selectedCell, setSelectedCell] = useState<Cell | null>(null)
+
+    const click = (cell: Cell) => {
+        if (cell.figure) {
+            setSelectedCell(cell)
+        }
+    }
+
+    const highlightCells = () => {
+        board.highlightCells(selectedCell)
+        updateBoard()
+    }
+
+    const updateBoard = () => {
+        const newBoard = board.getCopyBoard()
+        setBoard(newBoard)
+    }
+
     return (
         <div className={'board'}>
             {board.cells.map((row, index) =>
                 <Fragment key={index}>
                     {row.map(cell =>
                         <CellComponent
+                            click={click}
                             cell={cell}
                             key={cell.id}
+                            selected={cell.x === selectedCell?.x && cell.y === selectedCell?.y}
                         />
                     )}
                 </Fragment>
